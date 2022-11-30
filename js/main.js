28c777dc32e5071fdb22c5f3f7a70bea540f8e06
saddam <saddam.hussain@gmx.com>
2022-11-30 07:23:09 +0100

    js-file v-2

--- js/main.js
+++ js/main.js
@@ -1,48 +1,36 @@
-let menu = prompt("Welcome! \nPlease select one of the following options.\n For balance inquiry enter 1 \n For desposite enter 2 \n For withdrawal enter 3 \n To check account holder enter 4 \n For Exit enter 5");
-function atm(){
-    console.log(menu);
-}
-atm();
-
-let account = {accountName: "Oscar Jones", balance: 200,
+/*********************Object***********************/
+const account = {accountName: "Oscar Jones", balance: 200, exitCode: 0,
 getBalance(){
-    alert("Your current balance is: " +this.balance + " Kr");
-    alert("Thank You for using our services!");
+    alert(`Your current balance is:  ${this.balance} Kr`);
 },
+//Deposite function
 deposite(){
     let depositeAmount = parseInt(prompt("How much would you like to deposite?")); 
-    if (depositeAmount > 1){
-        account.deposite = depositeAmount;
-        alert("You have deposited: " +this.deposite + " Kr");
+    if (depositeAmount > 0){
+        alert(`You have deposited: ${depositeAmount} Kr`);
         this.balance += depositeAmount;
-        alert("Your current balance is now: " +this.balance + " Kr");
-        alert("Thank You for using our services!");
-    }
-    else if (depositeAmount > 1){
-        this.deposite = depositeAmount;
-        alert("You have deposited: " +this.deposite + " Kr");
-        this.balance += depositeAmount;
-        alert("Your current balance is now: " +this.balance + " Kr");
+        alert(`Your current balance is now: ${this.balance} Kr`);
         alert("Thank You for using our services!");
     }
     else if (depositeAmount <= 0){        
-            alert("Please enter positive value/amount. \nThank You!")
+            alert("Please enter positive value/amount and try again. \nThank You!")
         }
     else{
         alert("Please enter digits. \nThank You!")
     } 
-},
+},//Deposite function end
+
+//withdrawal function
 withdrawal(){
     let withdrawalAmount = parseInt(prompt("How much would you like to withdraw?"));
-    if (withdrawalAmount > 1 && withdrawalAmount <= this.balance){
-        this.withdrawal = withdrawalAmount;
-        alert("Your withdrawal amount is: " +this.withdrawal + " Kr");
+    if (withdrawalAmount >= 1 && withdrawalAmount <= this.balance){
+        alert(`Your withdrawal amount is: ${withdrawalAmount} Kr`);
         this.balance -= withdrawalAmount;
-        alert("Your current balance is now: " +this.balance + " Kr");
+        alert(`Your current balance is now: ${this.balance} Kr`);
         alert("Thank You for using our services!")
     }
     else if (withdrawalAmount <= 0){
-        alert("Please enter positive value/amount. \nThank You!")
+        alert("Please enter positive value/amount and try again. \nThank You!")
     }
     else if (withdrawalAmount > this.balance){
         alert("You don't have enough amount in your account. Please check your balance and try again. \nThank You!")
@@ -52,36 +40,73 @@ withdrawal(){
     }
 },
 getAccountName(){
-    alert("Account holder is: " +this.accountName);
-},
+    alert(`Account holder is: ${this.accountName}`);
+},//withdrawal function end
+
+//account-error function
 accountError(){
     alert("You entered a wrong key. Please choose from 1 to 5 and try again. \nThank You!");
-},
+},//account-error function end
+
+//exit-account function
 exitAccount(){
-    alert("Are you sure to logout?");
-    alert("Thank You for using our services!");
-}
+    if (confirm("Are you sure to logout?")){
+        alert("Thank You for using our services!");        
+        this.exitCode = 1;
+    }
+    else {
+        this.exitCode = 0;
+    }     
+}//exit-account function end
 };
-if (menu==1){
-    account.getBalance();
-}
-else if (menu==2){
-    account.deposite();
-}
-else if (menu==3){
-    account.withdrawal();
-}
-else if (menu==4){
-  account.getAccountName();  
-}
-else if (menu==5){
-    account.exitAccount();    
-}
-else {
- account.accountError();    
-}
+/*********************Object end***********************/
 
+/*********************show Menu function***********************/
+function showMenu(){
+    let menu = parseInt(prompt(`Welcome to banking services!
+    Please select one of the following options.
+    For balance inquiry, please enter 1.
+    For desposite, please enter 2.
+    For withdrawal, please enter 3.
+    To check account holder, please enter 4. 
+    For Exit, please enter 5.`));
+    return menu;
+}
+/*********************show Menu function end***********************/
 
+/*********************atm Function/to take inputs/choices from user***********************/
+function atm(input){ //strict equality (===) operator to compare and checks the data types. 
+    if (input===1){
+        account.getBalance();
+    }
+    else if (input===2){
+        account.deposite();
+    }
+    else if (input===3){
+        account.withdrawal();
+    }
+    else if (input===4){
+      account.getAccountName();  
+    } else if (input===5) {
+        account.exitAccount();
+    }
+    else {
+     account.accountError();    
+    }
+}
+/*********************atm Function/to take inputs/choices from user end***********************/
+
+/******************while loop/show menu to the user**********************/
+while (true) {
+    const input = showMenu();
+    atm(input);
+    if (account.exitCode){
+        break;
+    } else {
+        continue;
+    }
+}
+/******************while loop/show menu to the user end**********************/
 
 
 
